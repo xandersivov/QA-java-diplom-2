1c94ee83f545ed2f5bc8d63db2b909cd97db3e30
Alexander Sivov <Xandersivov@yandex.ru>
2022-05-10 15:31:35 +0700

    fix

--- src/test/java/UserUpdateTest.java
+++ src/test/java/UserUpdateTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class UserUpdateTese {
+public class UserUpdateTest {
 
     private UserData userData;
     private UserApi userApi;
